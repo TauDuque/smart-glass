9945b2cf517d9538ab634024b8d707295e98fa0e
Tau <hectauduque@gmail.com>
2020-12-04 15:24:07 -0300

    adding starting project

---
+++ controllers/Image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '125727fe6ae7453faf6c078b88a570d1'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(
+    "d02b4508df58432fbb84e800597b8959", 
+    req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work this API!!'))
+}
+
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries =>{ 
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('error'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
